02e11adaf00b25e611b037a0bd8b35afc7b981d1
Atul Jain <atuljain@Atuls.local>
2021-05-28 15:24:46 -0400

    Adding Program.Cs

---
+++ SomeGames/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SomeGames
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git World!");
+        }
+    }
+}
